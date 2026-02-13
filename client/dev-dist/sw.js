81f4271aea6007c0b413b6a98ac863912fe0d259
Segun <coolshegz@gmail.com>
2026-02-13 10:07:20 +0000

    var fix

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.cs6vdph8d0g"
+    "revision": "0.hibav723vrs"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
