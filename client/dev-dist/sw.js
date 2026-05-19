773f2486ba190ac3ff9c6fdfe65ff96ef9740598
Segun <coolshegz@gmail.com>
2026-05-19 23:18:15 +0100

    chore(client): rebuild dist with kinetic captions + jobs ui fix

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -81,12 +81,13 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "url": "registerSW.js",
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
-    "url": "index.html",
-    "revision": "0.tce8lm8ugm"
+    "url": "/index.html",
+    "revision": "0.ib9eprqv40s"
   }], {});
   workbox.cleanupOutdatedCaches();
-  workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
-    allowlist: [/^\/$/]
+  workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
+    allowlist: [/^\/$/],
+    denylist: [/^\/api/, /^\/outputs/, /^\/assets\/.+\.(js|css)$/]
   }));
 
 }));
