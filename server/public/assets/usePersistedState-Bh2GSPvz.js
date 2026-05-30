a5d5bc5532caed3e7c9e6e21fecc0de6b0e94a86
Segun <coolshegz@gmail.com>
2026-05-30 12:48:52 +0100

    build(client): rebuild bundle for synced-backgrounds toggle (#4)

---
+++ server/public/assets/usePersistedState-Bh2GSPvz.js
@@ -0,0 +1,1 @@
+import{r as e}from"./index-BnlcMydd.js";import{l as r,s as u}from"./storage-Cvxzc0GF.js";function c(s,a){const[t,o]=e.useState(()=>r(s,a));return e.useEffect(()=>{u(s,t)},[s,t]),[t,o]}export{c as u};
