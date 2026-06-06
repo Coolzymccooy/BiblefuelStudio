393105e53d1ef3aa505187033691837a55e968c7
Segun <coolshegz@gmail.com>
2026-06-06 11:07:21 +0100

    build(client): rebuild bundle with generative visuals + Ken Burns

---
+++ server/public/assets/Input-CKvGYcB0.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-C_AL7nBj.js";function r({className:t="",...n}){return s.jsx("input",{className:`input ${t}`,...n})}export{r as I};
---
+++ server/public/assets/Select-CDvIAOnV.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-C_AL7nBj.js";function n({className:t="",children:e,...r}){return s.jsx("select",{className:`input cursor-pointer ${t}`,...r,children:e})}export{n as S};
