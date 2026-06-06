bcc906eb8a7a993cd0d3b7013ea612bf7d9648b6
Segun <coolshegz@gmail.com>
2026-06-06 01:08:03 +0100

    build(client): rebuild bundle after final secondary-text cleanup

---
+++ server/public/assets/Select-CoiM2mbU.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-Cwxb75rn.js";function n({className:t="",children:e,...r}){return s.jsx("select",{className:`input cursor-pointer ${t}`,...r,children:e})}export{n as S};
--- server/public/assets/Textarea-CNsZeoaH.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-K9lH_qY9.js";function s({className:t="",...a}){return e.jsx("textarea",{className:`input min-h-[92px] ${t}`,...a})}export{s as T};
